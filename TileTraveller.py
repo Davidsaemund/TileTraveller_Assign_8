c386e29e634c962ad3c754fffa0979e0f3c42657
arnakaldal <arnak19@ru.is>
2020-09-17 14:36:28 +0000

    Fyrsta function

--- TileTraveller.py
+++ TileTraveller.py
@@ -5,12 +5,33 @@
 
 #Function that gets your current loc and tells you what options you have
 
+def tile_traveller(location):
+    if location == 1.1 or location == 2.1:
+        print("You can travel: (N)orth")
+    elif location == 1.2:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+    elif location == 2.2 or location == 3.3:
+        print("You can travel: (S)outh or (W)est.")
+    elif location == 1.3:
+        print("You can travel: (S)outh or (E)ast.")
+    elif location == 2.3:
+        print("You can travel: (E)ast or (W)est.")
+    elif location == 3.2:
+        print("You can travel: (N)orth or (S)outh")
+    elif location == 3.1:
+        print("Victory!")
+    return location
 
 #Function that gets current loc and wanted movement returns the resulting position
 
 
 
 # Main Program
+
+location = 1
+
+while location!= 9:
+    if 
 
 # Starts in 1,1
 
