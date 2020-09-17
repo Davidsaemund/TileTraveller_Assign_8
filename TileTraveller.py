c41cbc974b5da605028a7f179d80e529c36745ee
davidsaemund <davidsae14@ru.is>
2020-09-17 14:12:53 +0000

    Beinagrind

--- TileTraveller.py
+++ TileTraveller.py
@@ -2,3 +2,23 @@
 # Tile Traveler
 # Program that moves NORTH, SOUTH, WEST and EAST in a 3v3 tile plan.
 
+
+#Function that gets your current loc and tells you what options you have
+
+
+#Function that gets current loc and wanted movement returns the resulting position
+
+
+
+# Main Program
+
+# Starts in 1,1
+
+# while not winnging
+
+# ask function where we are and get directions
+
+# Tell program where you want to go
+
+# function that performs action if possible and return the resulting position
+
